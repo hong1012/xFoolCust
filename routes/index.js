6c56eeb9676237048c52a9c9a837d7f9b61df4e3
hong <254577340@qq.com>
2016-11-07 17:02:43 +0800

    nodejs

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: '上传图片' });
-    res.render('index', { title: '<h1>Express</h1>'
+    res.render('index', { title: 'xFoolCust'
         ,users:[{username: 'Wilson'},
             {username: 'Wilson Zhong'},
             {username: 'Zhong Wei'}]
--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
+var http = require('http');
+
+
+var querystring = require('querystring');
+
+
+
 /* GET users listing. */
 router.get('/get', function (req, res, next) {
 
@@ -30,6 +38,73 @@ router.post('/post', function (req, res, next) {
     }});
     res.end();
 
+
+});
+
+
+router.get('/get2', function (req, res, next) {
+
+    var contents = querystring.stringify({
+        eid: '4016572',
+        startdate: '2016-11-01',
+        enddate: '2016-11-08',
+        operateid: 1
+    });
+
+    var options = {
+        host: 'mob.cmcloud.cn',
+        path: '/serverCloud/DataReport/GetVisitDataTotal',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length' : contents.length
+        }
+    };
+
+    var req2 = http.request(options, function(res2) {
+        res2.setEncoding('utf8');
+        res2.on('data', function (data) {
+            console.log(data);
+            res.send(data);
+            res.end();
+        });
+    });
+    req2.write(contents);
+    req2.end();
+
+});
+
+router.get('/post2', function (req, res, next) {
+
+    var contents = querystring.stringify({
+        eid: '3975354',
+        appid: '791392829494',
+        secret: 'f6026839',
+        startDate: '2016-06-01',
+        endDate: '2016-11-08',
+        type: 0
+    });
+
+    var options = {
+        host: 'ebx.youshang.com',
+        path: '/thirdapi/doc/list',
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length' : contents.length
+        }
+    };
+
+    var req2 = http.request(options, function(res2) {
+        res2.setEncoding('utf8');
+        res2.on('data', function (data) {
+            console.log(data);
+            res.send(data);
+            res.end();
+        });
+    });
+    req2.write(contents);
+    req2.end();
 
 });
 
